20811ca1e473410a33d840793d0547e51742749d
First_damo <samiranbisal775@gmail.com>
2023-11-18 19:42:30 +0530

    File updates

--- Background color change/src/App.jsx
+++ Background color change/src/App.jsx
@@ -8,18 +8,18 @@ function App() {
   return (
     <>
       <h1 className='headtag'>Hello</h1>
-      <div className='head1' style={{ backgroundColor: color }} >
-        Hi Samiran
-        <div className='b1'>
-          <div className='b2'>
-            <button className='s1 s0' onClick={() => setColor('red')}>Red</button>
-            <button className='s1 s11' onClick={() => setColor('white')}>White</button>
-            <button className='s1 s2' onClick={() => setColor('black')}>Black</button>
-            <button className='s1 s3' onClick={() => setColor('green')}>green</button>
-            <button className='s1 s4' onClick={() => setColor('blue')}>Blue</button>
+        <div className='head1'style={{backgroundColor: color}} >
+          Hi Samiran
+          <div className='b1'>
+            <div className='b2'>
+              <button className='s1 s0' onClick={ () => setColor('red')}>Red</button>
+              <button className='s1 s11' onClick={ () => setColor('white')}>White</button>
+              <button className='s1 s2' onClick={ () => setColor('black')}>Black</button>
+              <button className='s1 s3' onClick={ () => setColor('green')}>green</button>
+              <button className='s1 s4' onClick={ () => setColor('blue')}>Blue</button>
+            </div>
           </div>
         </div>
-      </div>
     </>
   )
 }
